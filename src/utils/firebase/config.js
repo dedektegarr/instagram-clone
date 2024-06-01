03685b4c6db155676a3375c286381b3fc9b6389f
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-06-01 11:14:10 +0700

    refact: move firebase config to env

--- src/utils/firebase/config.js
+++ src/utils/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyDX3YOjeWyDjTRewJ5jWkIk--qoZOVid70",
-  authDomain: "our-project-a81b8.firebaseapp.com",
-  projectId: "our-project-a81b8",
-  storageBucket: "our-project-a81b8.appspot.com",
-  messagingSenderId: "310279443953",
-  appId: "1:310279443953:web:10ab84809d3ac3d1a76050",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
